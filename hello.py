5eb3b93196d9411e815805ace7ae57fb60dbfcaa
Jeff Lo <jeff@x220.localhost>
2017-05-15 15:37:21 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import requests
+ 
+app = Flask(__name__)
+
+@app.route("/bot", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'GET':
+        return "Hello World from flask"
+    else:
+	payload = {"bot_id": "f9b366898c181f1f3ef76da9f6", "text": "Hello world"}
+	r = requests.post("https://api.groupme.com/v3/bots/post", data=payload)
+
+
+if __name__ == "__main__":
+    app.run()
