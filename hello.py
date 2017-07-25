f0c5a7c68131f979d13560c19a19b22a8afee8c0
Jeff Lo <jeff@ve12sus.com>
2017-07-25 11:05:40 -0400

    hello.py testing with printed response

--- hello.py
+++ hello.py
@@ -1,17 +1,17 @@
 from flask import Flask, request
-import requests, json, re
+import requests, json
 import dial
- 
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == 'GET':
-        pass
+        return 'Get'
     elif request.method == 'POST':
-        req_data = request.get_json()
-        return dial.parse(req_data)
-    return 'okay'
+        resp = request.get_json(force=True)
+        print resp
+        return 'Post'
 
 if __name__ == "__main__":
     app.run()
